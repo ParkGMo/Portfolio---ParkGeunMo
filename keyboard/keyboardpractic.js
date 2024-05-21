de17913506b136fe76b81590a17b72d5ca725d58
sooyeon00 <167054991+sooyeon00@users.noreply.github.com>
2024-05-21 16:52:14 +0900

    5.21.16:52

--- keyboard/keyboardpractic.js
+++ keyboard/keyboardpractic.js
@@ -1,1 +1,39 @@
 const inputTag = document.querySelector("#my-input");
+const keyTag = document.querySelector("#key");
+const codeTag = document.querySelector("#code");
+const keypress = document.querySelector("#keypress");
+const keydown = document.querySelector("#keydown");
+const keyup = document.querySelector("#keyup");
+inputTag.focus();
+
+function keypressEvent(e) {
+  //   console.log(`key : ${e.key}, code : ${e.code}`);
+  keypress.classList.add("on");
+  setTimeout(function () {
+    keypress.classList.remove("on");
+  }, 300);
+}
+function keydownEvent(e) {
+  //   console.log(`key : ${e.key}, code : ${e.code}`);
+  const el = document.querySelector(`#${e.code}`);
+  codeTag.innerHTML = `key : ${e.code}`;
+  keyTag.innerHTML = `code : ${e.key}`;
+  el.classList.add("btnon");
+  keydown.classList.add("on");
+  setTimeout(function () {
+    keydown.classList.remove("on");
+  }, 300);
+}
+function keyupEvent(e) {
+  //   console.log(`key : ${e.key}, code : ${e.code}`);
+  const el = document.querySelector(`#${e.code}`);
+  el.classList.remove("btnon");
+  keyup.classList.add("on");
+  setTimeout(function () {
+    keyup.classList.remove("on");
+  }, 300);
+}
+
+inputTag.addEventListener("keypress", keypressEvent);
+inputTag.addEventListener("keydown", keydownEvent);
+inputTag.addEventListener("keyup", keyupEvent);
